93a7074920c328bcb86e336139cb68ab9257b758
Heitor Pires <heitorfernandes_3@hotmail.com>
2021-12-19 16:01:30 -0300

    implementando conceito de Value Object

--- MyStore.Domain/StoreContext/Entities/Customer.cs
+++ MyStore.Domain/StoreContext/Entities/Customer.cs
@@ -1,34 +1,34 @@
+using MyStore.Domain.StoreContext.ValueObjects;
+using System.Collections.Generic;
+
 namespace MyStore.Domain.StoreContext.Entities
 {
     public class Customer
-  {
-    public Customer(
-      string firstName,
-      string lastName,
-      string document,
-      string email,
-      string phone,
-      string address
-    )
     {
-      FirstName = firstName;
-      LastName = lastName;
-      Document = document;
-      Email = email;
-      Phone = phone;
-      Address = address;
-    }
-    public string FirstName { get; private set; }
-    public string LastName { get; private set; }
-    public string Document { get; private set; }
-    public string Email { get; private set; }
-    public string Phone { get; private set; }
-    public string Address { get; private set; }
+        public Customer(
+          Name name,
+          Document document,
+          Email email,
+          string phone   
+        )
+        {
+            Name = name;
+            Document = document;
+            Email = email;
+            Phone = phone;
+            Addresses = new List<Address>();
+        }
 
-    public override string ToString()
-    {
-      return $"{FirstName} {LastName}";
-    }
+        public Name Name { get; set; }
+        public Document Document { get; private set; }
+        public Email Email { get; private set; }
+        public string Phone { get; private set; }
+        public IReadOnlyCollection<Address> Addresses { get; private set; }
 
-  }
+        public override string ToString()
+        {
+            return Name.ToString();
+        }
+
+    }
 }
---
+++ MyStore.Domain/StoreContext/Enums/EAdressType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Domain.StoreContext.Enums
+{
+    public enum EAdressType
+    {   
+        Shipping = 1,
+        Billing = 2
+    }
+}
