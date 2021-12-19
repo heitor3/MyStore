00ffd632716a36a90bb2cdf35984251f29d9beac
Heitor Pires <heitorfernandes_3@hotmail.com>
2021-12-19 17:05:56 -0300

    finalizando entidades

--- MyStore.Domain/StoreContext/Entities/Order.cs
+++ MyStore.Domain/StoreContext/Entities/Order.cs
@@ -1,11 +1,14 @@
 using MyStore.Domain.StoreContext.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MyStore.Domain.StoreContext.Entities
 {
     public class Order
     {
+        private readonly IList<OrderItem> _items;
+        private readonly IList<Delivery> _deliveries;
 
         public Order(Customer customer)
         {
@@ -13,24 +16,27 @@ namespace MyStore.Domain.StoreContext.Entities
             Number = Guid.NewGuid().ToString().Replace("-", "").Substring(0,8).ToUpper();
             CreateDate = DateTime.Now;
             Status = EOrderStatus.Created;
-            Items = new List<OrderItem>();
-            Deliveries = new List<Delivery>();
+            _items = new List<OrderItem>();
+            _deliveries = new List<Delivery>();
         }
 
         public Customer Customer { get; private set; }
         public string Number { get; private set; }
         public DateTime CreateDate { get; private set; }
         public EOrderStatus Status { get; private set; }
-        public IReadOnlyCollection<OrderItem> Items { get; private set; }
-        public IReadOnlyCollection<Delivery> Deliveries { get; private set; }
+        public IReadOnlyCollection<OrderItem> Items => _items.ToArray();
+        public IReadOnlyCollection<Delivery> Deliveries => _deliveries.ToArray();
 
         public void AddItem(OrderItem item)
         {
+            _items.Add(item);
+        }
 
+        public void AddDelivery(Delivery delivery)
+        {
+            _deliveries.Add(delivery);
         }
 
         public void Place() { }
     }
 }
-
-//video 14
