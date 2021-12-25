ff30fd17e3f80ba2452d69e8b29c4cacf778e779
Heitor Pires <heitorfernandes_3@hotmail.com>
2021-12-24 22:51:26 -0300

    criando commands

---
+++ MyStore.Domain/StoreContext/Commands/OrderCommands/Inputs/PlaceOrderCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Domain.StoreContext.Commands.OrderCommands.Inputs
+{
+    public class PlaceOrderCommand
+    {
+        public Guid Customer { get; set; }
+        public IList<OrderItemCommand> OrderItems { get; set; }
+    }
+
+    public class OrderItemCommand
+    {
+        public Guid Product { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
